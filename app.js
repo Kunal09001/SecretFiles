09b3c9f9e103467451e361339b646dc06a70e39c
Kunal09001 <kunalgangaramani.kg@gmail.com>
2022-07-01 01:22:13 +0530

    oAuth using google and github

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 
 app.use(express.static("public"));
@@ -31,21 +32,81 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema ({
     email : String,
-    password : String
+    password : String,
+    googeId : String,
+    secret : String
 });
 
 userSchema.plugin(passportLocalMongoose);
+
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id); 
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      console.log(user);
+      return cb(err, user);
+    });
+  }
+));
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
     res.render("home");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets', 
+    passport.authenticate('google',{ 
+        failureRedirect: '/login' 
+    }),
+    function(req, res) {
+// Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+});  
+
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/secrets', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get("/login",function(req,res){
     res.render("login");
@@ -56,11 +117,13 @@ app.get("/register",function(req,res){
 });
 
 app.get("/secrets",function(req,res){
-    if(req.isAuthenticated()){
-        res.render("secrets");    
-    }else{
-        res.redirect("login");
-    }
+   User.find({"secret " : {$ne : null} },function(err,foundUsers){
+        if(!err){
+            if(foundUsers){
+                res.render("secrets",{userWithSecrets : foundUsers});
+            }
+        }
+   });
 });
 
 app.get("/logout",function(req,res){
@@ -71,6 +134,14 @@ app.get("/logout",function(req,res){
             res.redirect("/");    
     });
 });
+
+app.get("/submit",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("submit");    
+    }else{
+        res.redirect("login");
+    }
+})
 
 app.post("/register",function(req,res){
    User.register({username:req.body.username},req.body.password,function(err,user){
@@ -102,6 +173,20 @@ app.post("/login",function(req,res){
     });
 
 });
+
+app.post("/submit",function(req,res){
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id,function(err,foundUser){
+        if(!err){
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    })
+})
 
 app.listen(3000,function(){
     console.log("Port is started at 3000");
